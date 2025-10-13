c9b6f94794a9c8a763faa74bf12b6a2c8bb6593a
DvcAnh <161046512+DucAnh0212@users.noreply.github.com>
2025-10-13 23:09:52 +0700

    OrderItems.java

--- OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/models/OrderItems.java
+++ OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/models/OrderItems.java
@@ -9,64 +9,74 @@ import java.sql.SQLException;
 public class OrderItems {
     private int id;
     private int orderid;
-    private String menu_item_name;
+    private int menu_item_id;
     private int quantity;
     private int price;
 
-    public OrderItems(int id, int orderid, String menu_item_name, int quantity, int price){
-        this.id=id;
-        this.orderid=orderid;
-        this.menu_item_name=menu_item_name;
-        this.quantity=quantity;
-        this.price=price;
+    public OrderItems(){
     }
-
-    public int getId() { return id;}
-    public void setId(int id) {this.id = id;}
-
-    public int getOrderId() { return orderid;}
-    public void setOrderid(int orderid) {this.orderid = orderid;}
-
-    public String getMenuItemName() { return menu_item_name;}
-    public void setMenu_item_name(String menu_item_name) {this.menu_item_name = menu_item_name;}
-
-    public int getQuantity() { return quantity;}
-    public void setQuantity(int quantity) {this.quantity = quantity;}
-
-    public int getPrice() { return price;}
-    public void setPrice(int price) {this.price = price;}
-
+    public OrderItems(int id, int orderid, int menu_item_id, int quantity, int price){
+        this.id = id;
+        this.orderid = orderid;
+        this.menu_item_id = menu_item_id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getOrderId() {
+        return orderid;
+    }
+    public void setOrderId(int a) {
+        this.orderid = a;
+    }
+    public int getMenu_item_id() {
+        return menu_item_id;
+    }
+    public void setMenu_item_id(int menu_item_id) {
+        this.menu_item_id = menu_item_id;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
     public void save() throws SQLException {
-        String sql = "INSERT INTO OrderItems(ORDER_ID, MENU_ITEM_NAME, QUANTITY, PRICE) VALUES (?, ?, ?, ?)";
-
+        String sql = "INSERT INTO OrderItems(ORDER_ID, MENU_ITEM_ID, QUANTITY, PRICE) VALUES (?, ?, ?, ?)";
         try (Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
             preparedStatement.setInt(1, this.orderid);
-            preparedStatement.setString(2, this.menu_item_name);
+            preparedStatement.setInt(2, this.menu_item_id);
             preparedStatement.setInt(3, this.quantity);
             preparedStatement.setInt(4, this.price);
-
             preparedStatement.executeQuery();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public void update() throws SQLException {
         String sql = "UPDATE OrderItems SET QUANTITY = ? WHERE ID = ?";
-
         try(Connection connection = DatabaseConnector.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-
             preparedStatement.setInt(1, this.quantity);
             preparedStatement.setInt(2, this.id);
-
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
-
 
