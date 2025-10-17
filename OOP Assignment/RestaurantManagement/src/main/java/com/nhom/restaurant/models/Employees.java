f42b582d1761f99653916e9af66ab591df2a654f
tung2k5vm <167506718+tung2k5vm@users.noreply.github.com>
2025-10-17 18:36:22 +0700

    Update Employees.java

--- OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/models/Employees.java
+++ OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/models/Employees.java
@@ -26,7 +26,7 @@ public class Employees {
         return this.workshift;
     }
     public void setId(int a){
-        this.id = a;
+        if(a>0) this.id = a;
     }
     public void setFull_name(String a){
         this.full_name = a;
@@ -34,43 +34,5 @@ public class Employees {
     public void setWorkshift(String a){
         this.workshift = a;
     }
-    public static Employees findById(int id){
-        String sql = "SELECT * FROM Employees WHERE ID = ?";
-        Employees foundemployee = null;
-        try(Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(sql)){
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if(rs.next()){
-                foundemployee = new Employees();
-                foundemployee.setId(rs.getInt("ID"));
-                foundemployee.setFull_name(rs.getString("FULLNAME"));
-                foundemployee.setWorkshift(rs.getString("WORKSHIFT"));
-            }
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        return foundemployee;
-    }
-
-    public static List<Employees> findAll(){
-        String sql = "SELECT * FROM Employees";
-        List<Employees> allEmployee = new ArrayList<>();
-        try(Connection connection = DatabaseConnector.getConnection();
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)){
-            while(rs.next()){
-                Employees employee = new Employees();
-                employee.setId(rs.getInt("ID"));
-                employee.setFull_name(rs.getString("FULLNAME"));
-                employee.setWorkshift(rs.getString("WORKSHIFT"));
-                allEmployee.add(employee);
-            }
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        return allEmployee;
-    }
 }
+
