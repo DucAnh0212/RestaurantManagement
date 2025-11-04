17d96f190a2825e0e6449591e7b596318b182e66
DvcAnh <161046512+DucAnh0212@users.noreply.github.com>
2025-11-04 23:57:24 +0700

    Update ReportManager.java

--- OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/manager/ReportManager.java
+++ OOP Assignment/RestaurantManagement/src/main/java/com/nhom/restaurant/manager/ReportManager.java
@@ -12,7 +12,9 @@ public class ReportManager {
     public static DailyReport genDailyReport(LocalDate date){
         List<Orders> historyList = new ArrayList<>();
         int totalRevenue = 0;
-        String sql = "SELECT * FROM Orders WHERE DATE(CREATED_AT) = ?";
+        String sql = "SELECT O.*, E.FULLNAME FROM Orders O " +
+                     "JOIN Employees E ON O.EMPLOYEE_ID = E.ID " +
+                     "WHERE DATE(O.CREATED_AT) = ?";
         try(Connection connection = DatabaseConnector.getConnection();
             PreparedStatement pstmt = connection.prepareStatement(sql)){
             pstmt.setDate(1, Date.valueOf(date));
@@ -21,11 +23,15 @@ public class ReportManager {
                 Orders selectedOrder = new Orders();
                 selectedOrder.setId(rs.getInt("ID"));
                 selectedOrder.setTable_id(rs.getInt("TABLE_ID"));
+                selectedOrder.setEmployee_id(rs.getInt("EMPLOYEE_ID"));
+                selectedOrder.setEmployee_name(rs.getString("FULLNAME"));
                 selectedOrder.setStatus(rs.getString("STATUS"));
                 selectedOrder.setTotal_amount(rs.getInt("TOTAL_AMOUNT"));
                 selectedOrder.setCreated_at(rs.getTimestamp("CREATED_AT"));
+                if (selectedOrder.getStatus().equals("Đã thanh toán")) {
+                    totalRevenue += selectedOrder.getTotal_amount();
+                }
                 historyList.add(selectedOrder);
-                totalRevenue += selectedOrder.getTotal_amount();
             }
         }
         catch(SQLException e){
